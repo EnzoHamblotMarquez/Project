b04ca40f2fa8f86bb8dec4b53dacd8450befab29
EnzoHamblotMarquez <ehamblotm@edu.tecnocampus.cat>
2024-04-16 21:20:49 +0200

    Player Movement

---
+++ Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float movement = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-movement * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX=true;
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(movement * Time.deltaTime, 0, 0);
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKey("up"))
+        {
+            gameObject.transform.Translate(0, movement * Time.deltaTime, 0);
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+        }
+        if (Input.GetKey("down"))
+        {
+            gameObject.transform.Translate(0, -movement * Time.deltaTime, 0);
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+        }
+
+        if(!Input.GetKey("left") && !Input.GetKey("right") && !Input.GetKey("up") && !Input.GetKey("down"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("moving", false);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            movement = 2f;
+            gameObject.GetComponent<Animator>().SetBool("dash", true);
+        }
+        else
+        {
+            movement = 1f;
+            gameObject.GetComponent<Animator>().SetBool("dash", false);
+        }
+    }
+}
